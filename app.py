fbf1577aab3b60fcd027238187667f95944a1b8d
Eliot-shud <elyarkamol1630@gmail.com>
2023-03-21 12:25:49 +0700

    homework finished

--- app.py
+++ app.py
@@ -1,16 +1,8 @@
-import os
-
 from flask import Flask
-
-app = Flask(__name__)
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DATA_DIR = os.path.join(BASE_DIR, "data")
+from views import main_bp
 
 
-@app.post("/perform_query")
-def perform_query():
-    # нужно взять код из предыдущего ДЗ
-    # добавить команду regex
-    # добавить типизацию в проект, чтобы проходила утилиту mypy app.py
-    return app.response_class('', content_type="text/plain")
+def create_app() -> Flask:
+    app = Flask(__name__)
+    app.register_blueprint(main_bp)
+    return app
